afa9238f3bc25432c6c2242534b401574ab8344f
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-17 00:00:50 -0700

    Includes the definition of all the components of the testbench

---
+++ not_gate_verification/package.sv
@@ -0,0 +1,15 @@
+`include "seq_item.sv"
+
+`include "base_seq.sv"
+
+`include "sequencer.sv"
+
+`include "driver.sv"
+
+`include "monitor.sv"
+
+`include "scoreboard.sv"
+
+`include "agent.sv"
+
+`include "env.sv"
