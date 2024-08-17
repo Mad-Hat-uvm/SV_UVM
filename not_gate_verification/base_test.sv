0716f32ea7ff95770acb206e1014d8054369e57d
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-16 17:36:33 -0700

    Body of base test

--- not_gate_verification/base_test.sv
+++ not_gate_verification/base_test.sv
@@ -6,6 +6,30 @@ class base_test extends uvm_test;
     env env_o;
     base_seq bseq;
 
-    function new(string name="env", uvm_component parent=null);
+    function new(string name="base_test", uvm_component parent=null);
         super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+
+        env_o = env::type_id::create("env_o", this);
+
+    endfunction
+
+    task run_phase(uvm_phase phase);
+
+        phase.raise_objection(this);
+
+        bseq = base_seq::type_id::create("bseq");
+
+        repeat(5)begin
+            #5;
+            bseq.start(env_o.agt.seqr);
+        end
+        phase.drop_objection(this);
+
+        `uvm_info(get_type_name(), "END_OF_TESTCASE", UVM_LOW);
+        
+    endtask
 endclass
