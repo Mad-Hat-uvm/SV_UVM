5f32e2210623d98b1754688e67ad9a67d2953855
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-21 16:10:41 -0700

    Update seqr.sv

--- Adder_Verification/seqr.sv
+++ Adder_Verification/seqr.sv
@@ -1,8 +1,12 @@
-class my_sequencer extends uvm_sequencer #(data_item)
-  `uvm_component_utils(my_sequencer)
+class seqcr extends uvm_sequencer #(seq_item)
+  `uvm_component_utils(seqcr)
 
-  function new(string name, uvm_component parent);
+  function new(string name = "seqcr", uvm_component parent = null);
     super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
   endfunction
   
 endclass
