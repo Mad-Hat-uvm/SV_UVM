239828768aa6a08c7e726e247e77b7adb3511cad
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-21 16:30:25 -0700

    Update and rename agent.sv to active_agent.sv

--- Adder_Verification/active_agent.sv
+++ Adder_Verification/active_agent.sv
@@ -1,11 +1,11 @@
-class a_agent extends uvm_agent;
-  `uvm_component_utils(a_agent)
+class active_agent extends uvm_agent;
+  `uvm_component_utils(active_agent)
 
   driver drv;
   seqr sqr;
   monitor mon;
 
-  function new(string name = "a_agent", uvm_component parent = null);
+  function new(string name = "active_agent", uvm_component parent = null);
     super.new(name, parent);
   endfunction
 
@@ -28,21 +28,4 @@ class a_agent extends uvm_agent;
   
 endclass
 
-class b_agent extends uvm_agent;
-  `uvm_component_utils(b_agent)
 
-  monitor mon;
-
-  function new(string name = "b_agent", uvm_component parent = null);
-    super.new(name, parent);
-  endfunction
-
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    if(get_is_active() == UVM_PASSIVE) begin
-      mon = monitor::type_id::create("drv", this);
-      `uvm_info(get_type_name(), "This is a Passive Agent", UVM_LOW);
-    end
-  endfunction
-
-endclass
