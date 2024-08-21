144e2a9ad1de03f586146965dfd8ddbf33c7a008
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-21 16:08:20 -0700

    Update base_seq.sv

---
+++ Adder_Verification/base_seq.sv
@@ -0,0 +1,15 @@
+class base_seq extends uvm_sequencer #(seq_item)
+  `uvm_object_utils(base_seq)
+
+   seq_item req;
+  
+  function new(string name = "base_seq");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), "Base seq: Inside Body", UVM_LOW);
+    `uvm_do(req);
+  endtask
+  
+endclass
