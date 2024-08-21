8960d2c5aedf1035ed15b39eefc73146adf9b447
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-21 16:38:59 -0700

    Update scoreboard.sv

--- Adder_Verification/scoreboard.sv
+++ Adder_Verification/scoreboard.sv
@@ -1,7 +1,7 @@
 class scoreboard extends uvm_scoreboard;
   `uvm_component_utils(scoreboard)
  
-  uvm_analysis_port #(seq_item, scoreboard) item_collect_export;
+  uvm_analysis_imp #(seq_item, scoreboard) item_collect_export;
   seq_item item_q[$];
 
   function new(string name = "monitor", uvm_component parent = null);
@@ -22,8 +22,17 @@ class scoreboard extends uvm_scoreboard;
     seq_item sb_item;
     forever begin
       wait(item_q.size > 0) begin
-        sb_item = item_q.pop_front();
-        //Checking comparing logic
+
+        if(item_q.size > 0) begin
+          sb_item = item_q.pop_front();
+          $display("--------------------------------------------------------------------------------------------------------------------------------");
+          if(sb_item.ip1 + sb_item.ip2 = sb_item.out) begin
+            `uvm_info(get_type_name(), $sformatf("Matched: ip1 = %0d, ip2 = %0d , out = %0d",sb_item.ip1, sb_item.ip2, sb_item.out), UVM_LOW);
+          end
+          else begin
+            `uvm_error(get_type_name(), $sformatf("Not Matched: ip1 = %0d, ip2 = %0d , out = %0d",sb_item.ip1, sb_item.ip2, sb_item.out);
+          end
+         $display("--------------------------------------------------------------------------------------------------------------------------------");
       end
     end
   endtask
