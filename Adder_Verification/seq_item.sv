bd38bb6288c04a3f58dd35a92251c57616585cc5
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-21 16:11:20 -0700

    Update seq_item.sv

--- Adder_Verification/seq_item.sv
+++ Adder_Verification/seq_item.sv
@@ -2,8 +2,8 @@ class seq_item extends uvm_sequence_item;
   rand bit [7:0] ip1, ip2;
   bit [8:0] out;
 
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
+  function new(string name = "seq_item");
+    super.new(name);
   endfunction
 
   `uvm_object_utils_begin(seq_item)
