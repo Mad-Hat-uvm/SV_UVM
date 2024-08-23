fb1cf10c9804e162a66ccfed8e1b56dc02b102fb
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-23 12:00:58 -0700

    Update seq_item.sv

--- Adder_Verification/seq_item.sv
+++ Adder_Verification/seq_item.sv
@@ -10,7 +10,7 @@ class seq_item extends uvm_sequence_item;
    `uvm_field_int(ip1,UVM_ALL_ON)
    `uvm_field_int(ip2,UVM_ALL_ON)
    `uvm_field_int(out,UVM_ALL_ON)
-  `uvm_object_utils_end(seq_item)
+  `uvm_object_utils_end
 
   constraint ip_c {ip1 < 100; ip2 < 100;}
   
