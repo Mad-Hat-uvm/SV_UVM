3d27bbde03901ac59250223a623e78326f9c5d23
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-23 15:20:41 -0700

    Update seq_item.sv

--- Adder_Verification/seq_item.sv
+++ Adder_Verification/seq_item.sv
@@ -1,3 +1,6 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
 class seq_item extends uvm_sequence_item;
   rand bit [7:0] ip1, ip2;
   bit [8:0] out;
@@ -7,9 +10,9 @@ class seq_item extends uvm_sequence_item;
   endfunction
 
   `uvm_object_utils_begin(seq_item)
-   `uvm_field_int(ip1,UVM_ALL_ON)
-   `uvm_field_int(ip2,UVM_ALL_ON)
-   `uvm_field_int(out,UVM_ALL_ON)
+   `uvm_field_int(ip1,UVM_DEFAULT)
+   `uvm_field_int(ip2,UVM_DEFAULT)
+   `uvm_field_int(out,UVM_DEFAULT)
   `uvm_object_utils_end
 
   constraint ip_c {ip1 < 100; ip2 < 100;}
