7c0ef26eb9789f52531b845f95bf39552643a83f
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-23 16:24:24 -0700

    Update seqr.sv

--- Adder_Verification/seqr.sv
+++ Adder_Verification/seqr.sv
@@ -5,7 +5,7 @@ class seqr extends uvm_sequencer #(seq_item);
     super.new(name, parent);
   endfunction
 
-  function void build_phase(uvm_phase phase);
+  virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
   endfunction
   
