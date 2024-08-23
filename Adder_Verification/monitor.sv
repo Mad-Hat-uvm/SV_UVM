92710da29e11700cf9bacd87cce6b55be23aae62
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-23 16:11:41 -0700

    Update monitor.sv

--- Adder_Verification/monitor.sv
+++ Adder_Verification/monitor.sv
@@ -7,25 +7,25 @@ class monitor extends uvm_monitor;
 
   function new(string name = "monitor", uvm_component parent = null);
     super.new(name, parent);
-    item_collect_port = new("item_collect_port", this);
-    mon_item = new();
+    item_collect_port = new("Write", this);
   endfunction
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+    mon_item = seq_item::type_id::create("TRANS");
     if(!uvm_config_db#(virtual add_if) :: get(this, "", "vif", vif))
       `uvm_fatal(get_type_name(), "Interface not set at top level");
   endfunction
 
   task run_phase(uvm_phase phase);
+    @(negedge vif.reset);
     forever begin
-      wait(!vif.reset);
-      @(posedge vif.clk);
+      repeat(2)@(posedge vif.clk);
       mon_item.ip1 <= vif.ip1;
       mon_item.ip2 <= vif.ip2;
-      `uvm_info(get_type_name(), $sformatf("ip1 = %0d, ip2 = %0d", mon_item.ip1, mon_item.ip2), UVM_HIGH);
+      mon_item.out <= vif.out;
+      `uvm_info(get_type_name(), $sformatf("ip1 = %0d, ip2 = %0d, out: %d", mon_item.ip1, mon_item.ip2, mon_item.out), UVM_NONE);
       @(posedge vif.clk);
-        mon_item.out <= vif.out;
       item_collect_port.write(mon_item);
     end
   endtask
