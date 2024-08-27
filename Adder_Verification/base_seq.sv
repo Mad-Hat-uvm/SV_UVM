7067ec85f2009ff60849060a7e43cfba63490f91
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-08-26 17:48:42 -0700

    Update base_seq.sv

--- Adder_Verification/base_seq.sv
+++ Adder_Verification/base_seq.sv
@@ -11,9 +11,9 @@ class base_seq extends uvm_sequence#(seq_item);
     req = seq_item::type_id::create("req");
      repeat(10)begin
       start_item(req);
-    `uvm_info(get_type_name(), $sformatf("Data sent to Driver ip1: %0d,ip2: %d",req.ip1, req.ip2), UVM_NONE);
-    req.randomize();
-    finish_item(req);
+      req.randomize();
+      finish_item(req);
+       `uvm_info(get_type_name(), $sformatf("Data sent to Driver ip1: %0d,ip2: %d",req.ip1, req.ip2), UVM_NONE);
     end
 
   endtask
