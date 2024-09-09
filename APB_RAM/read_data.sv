70300ec161fd0ca21598de494a0fe30e40b00d75
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-09-09 00:09:59 -0700

    Read_data sequence updated

---
+++ APB_RAM/read_data.sv
@@ -0,0 +1,20 @@
+class read_data extends uvm_sequence# (transaction);
+    `uvm_object_utils(read_data)
+
+    function new(string name = "read_data");
+        super.new(name);
+    endfunction
+
+    virtual task body();
+        repeat(15)
+         begin
+            tr = transaction::type_id::create("tr");
+            tr.addr_c.constraint_mode(1);
+            tr.addr_c_err.constraint_mode(0);
+            start_item(tr);
+            assert(tr.randomize);
+            tr.op = readd;
+            finish_item(tr);
+         end
+    endtask
+endclass
