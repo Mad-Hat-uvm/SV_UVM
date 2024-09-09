834dff41bd9487ddd862c3e74609fdf292904736
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-09-09 00:14:07 -0700

    write_read sequence

---
+++ APB_RAM/write_read.sv
@@ -0,0 +1,26 @@
+class write_read extends uvm_sequence# (transaction);
+    `uvm_object_utils(write_read)
+
+    function new(string name = "write_read");
+        super.new(name);
+    endfunction
+
+    virtual task body();
+        repeat(15)
+         begin
+            tr = transaction::type_id::create("tr");
+            tr.addr_c.constraint_mode(1);
+            tr.addr_c_err.constraint_mode(0);
+
+            start_item(tr);
+            assert(tr.randomize);
+            tr.op = writed;
+            finish_item(tr);
+
+            start_item(tr);
+            assert(tr.randomize);
+            tr.op = readd;
+            finish_item(tr);
+         end
+    endtask
+endclass
