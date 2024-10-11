e1d6a17a2b36a4f81603efca23d548c116485e32
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-10-11 01:22:26 -0700

    Added rd_data to transaction

--- FIFO/fifo_transaction.sv
+++ FIFO/fifo_transaction.sv
@@ -3,6 +3,7 @@ class fifo_transaction extends uvm_sequence_item;
     rand bit wr_en;
     rand bit rd_en;
     rand bit [7:0] wr_data;
+         bit [7:0] rd_data;
 
     //Prevent both read and write from being unabled at the same time
     constraint valid_op {!(wr_en && rd_en); }
@@ -17,10 +18,11 @@ class fifo_transaction extends uvm_sequence_item;
         `uvm_field_int(wr_en, UVM_ALL_ON)
         `uvm_field_int(rd_en, UVM_ALL_ON)
         `uvm_field_int(wr_data, UVM_ALL_ON)
+        `uvm_field_int(rd_data, UVM_ALL_ON)
     `uvm_object_utils_end
 
     function new(string name = "fifo_transaction");
         super.new(name);
     endfunction
-    
+
 endclass
