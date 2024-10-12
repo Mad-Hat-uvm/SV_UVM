9e9874e0dc5c1617a39c768b4120cce4ca4c6baa
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2024-10-12 03:01:57 -0700

    Modified scoreboard to include 2 separate analysis ports

--- FIFO/fifo_scoreboard.sv
+++ FIFO/fifo_scoreboard.sv
@@ -1,34 +1,31 @@
 class fifo_scoreboard extends uvm_scoreboard;
     `uvm_component_utils(fifo_scoreboard);
 
-    virtual fifo_if.sb vif;     //Use the sb modport
-    uvm_analysis_imp#(fifo_transaction, fifo_scoreboard) ap;
-    queue#(bit [7:0]) expected_fifo;
+    uvm_analysis_imp#(fifo_transaction, fifo_scoreboard) write_ap;
+    uvm_analysis_imp#(fifo_transaction, fifo_scoreboard) read_ap;
+
+    queue [7:0] fifo_queue;
 
     function new(string name = "fifo_scoreboard", uvm_component parent);
         super.new(name, parent);
-        ap =new("ap",this);
-        expected_fifo = new();
-    endfunction
+        write_ap =new("write_ap",this);
+        read_ap =new("read_ap",this);
 
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-        if(!uvm_config_db#(virtual fifo_if.sb)::get(this, "", "vif", vif))
-         `uvm_fatal("NOVIF", "No virtual interface for scoreboard found via uvm_config_db");
+        fifo_queue = new();
     endfunction
 
     virtual function void write(fifo_transaction tr);
         //Handle Write transactions
         if(tr.wr_en && !vif.full) begin
             // Push written data into reference model
-            expected_fifo.push_back(tr.wr_data);
+            fifo_queue.push_back(tr.wr_data);
             `uvm_info("FIFO_SB", $sformatf("Data written to FIFO: %0h", tr.wr_data), UVM_LOW);
         end
 
         //Handle Read transactions
         if(tr.rd_en && !vif.empty) begin
-            if(expected_fifo.size() > 0) begin
-                bit [7:0] expected_data = expected_fifo.pop_front();
+            if(fifo_queue.size() > 0) begin
+                bit [7:0] expected_data = fifo_queue.pop_front();
 
                 //Check if read data matches the earliest written unread data
                 if(tr.rd_data === expected_data) begin
