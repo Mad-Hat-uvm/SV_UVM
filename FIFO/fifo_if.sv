744d38f4de2ca80cfbed337d9fe5376c461a3520
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-03-29 00:15:21 -0700

    Basic fifo interface

--- FIFO/fifo_if.sv
+++ FIFO/fifo_if.sv
@@ -1,25 +1,7 @@
-interface fifo_if #(parameter DATA_WIDTH = 8, DEPTH = 16);
-
-    //Define signals
-    logic clk;
-    logic reset_n;
-    logic wr_en;
-    logic rd_en;
-    logic [DATA_WIDTH-1:0] wr_data;
-    logic [DATA_WIDTH-1:0] rd_data;
-    logic full;
-    logic empty;
-
-    //Modport for the driver: driver drives wr_en, rd_en and wr_data.
-    modport drv (output wr_en, output rd_en, output wr_data, input full, input empty, input rd_data, input clk, input reset_n);
-
-    //Modport for the monitor: Monitor observes everything but drives nothing
-    modport mon (input wr_en, input rd_en, input wr_data, input full, input empty, input rd_data, input clk, input reset_n);
-
-    //Modport for scoreboard: scoreboard reads relevant output signals from the FIFO.
-    modport sb(input rd_data, input clk, input reset_n);
-
-    //Modport for assertions or coverage to observesignals
-    modport cov (input wr_en, input rd_en, input full, input empty, input clk, input reset_n);
-    
+interface fifo_if #(parameter = 8);
+    logic clk, rst;
+    logic wr_en, rd_en;
+    logic [WIDTH-1 : 0] din;
+    logic [WIDTH-1 : 0] dout;
+    logic empty, full;
 endinterface
