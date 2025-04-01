45d9d71672c1c5b05c687b8e159b07f4cfbf3125
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-04-01 10:58:38 -0700

    Sequencer added to environment

--- FIFO/fifo_env.sv
+++ FIFO/fifo_env.sv
@@ -4,19 +4,22 @@ class fifo_env extends uvm_env;
  fifo_driver drv;
  fifo_monitor mon;
  fifo_scoreboard sb;
+ fifo_sequencer seqr;
 
  function new(string name, uvm_component parent);
     super.new(name, parent);
  endfunction
 
  function void build_phase(uvm_phase phase);
-    drv = fifo_driver::type_id::create("drv", this);
-    mon = fifo_monitor::type_id::create("mon", this);
-    sb  = fifo_scoreboard::type_id::create("sb", this);
+    drv  = fifo_driver::type_id::create("drv", this);
+    mon  = fifo_monitor::type_id::create("mon", this);
+    sb   = fifo_scoreboard::type_id::create("sb", this);
+    seqr = fifo_sequencer::type_id::create("seqr", this);
  endfunction
 
  function void connect_phase(uvm_phase phase);
+    drv.seq_item_port.connect(seqr.seq_item_export);
     mon.mon_ap.connect(sb.sb_port);
  endfunction
- 
+
 endclass
