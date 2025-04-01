a256465daca385fb992e5e43c9add6393e0b55b5
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-04-01 09:42:18 -0700

    Basic fifo environment

--- FIFO/fifo_env.sv
+++ FIFO/fifo_env.sv
@@ -1,33 +1,22 @@
 class fifo_env extends uvm_env;
-    `uvm_component_utils(fifo_env);
+   `uvm_component_utils(fifo_env)
 
-    fifo_write_agent write_agent;
-    fifo_read_agent read_agent;
-    fifo_scoreboard sb;
-    fifo_coverage cov_fifo;
+ fifo_driver drv;
+ fifo_monitor mon;
+ fifo_scoreboard sb;
 
-    function new(string name = "fifo_env", uvm_component parent);
-        super.new(name, parent);
-    endfunction
+ function new(string name, uvm_component parent);
+    super.new(name, parent);
+ endfunction
 
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-        
-        write_agent = fifo_write_agent::type_id::create("write_agent", this);
-        read_agent  = fifo_read_agent::type_id::create("read_agent", this);
-        sb          = fifo_scoreboard::type_id::create("sb", this);
-        cov_fifo        = fifo_coverage::type_id::create("cov_fifo", this);
+ function void build_phase(uvm_phase phase);
+    drv = fifo_driver::type_id::create("drv", this);
+    mon = fifo_monitor::type_id::create("mon", this);
+    sb  = fifo_scoreboard::type_id::create("sb", this);
+ endfunction
 
-    endfunction
-
-    function void connect_phase(uvm_phase phase);
-        super.connect_phase(uvm_phase phase);
-
-       write_agent.mon.ap.connect(sb.write_ap);
-       write_agent.mon.ap.connect(cov_fifo.analysis_export);
-
-       read_agent.mon.ap.connect(sb.read_ap);
-       read_agent.mon.ap.connect(cov_fifo.analysis_export);
-    endfunction
-    
+ function void connect_phase(uvm_phase phase);
+    mon.mon_ap.connect(sb.sb_port);
+ endfunction
+ 
 endclass
