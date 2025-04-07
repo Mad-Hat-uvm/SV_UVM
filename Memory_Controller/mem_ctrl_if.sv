a656874971b32ba5014cf967c3f8b019f2caeb77
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-04-07 11:06:43 -0700

    Delete Memory_Controller directory

--- Memory_Controller/mem_ctrl_if.sv
+++
@@ -1,9 +0,0 @@
-interface mem_ctrl_if(input logic clk, input logic reset_n);
-    logic [31:0] addr;
-    logic [31:0] wdata;
-    logic [31:0] rdata;
-    logic we;
-    logic re;
-    logic ready;
-endinterface
-
