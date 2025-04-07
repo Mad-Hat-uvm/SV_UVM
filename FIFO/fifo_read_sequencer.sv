f4b828959e1ac8b1eb06da1fce85aa8b9dad0868
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-04-07 11:12:29 -0700

    Delete FIFO/fifo_read_sequencer.sv

--- FIFO/fifo_read_sequencer.sv
+++
@@ -1,8 +0,0 @@
-class fifo_read_sequencer extends uvm_sequencerr#(fifo_transaction);
-    `uvm_component_utils(fifo_read_sequencer)
-
-    function new(string name = "fifo_read_sequencer", uvm_component parent);
-        super.new(name, parent);
-    endfunction
-
-endclass
