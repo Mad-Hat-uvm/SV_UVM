cab28e19146e60cae33012dce88dd95ebedb271d
Mad-Hat-uvm <yamini.bitmesra@gmail.com>
2025-04-07 11:06:00 -0700

    Delete APB_RAM directory

--- APB_RAM/apb_config.sv
+++
@@ -1,9 +0,0 @@
-class apb_config extends uvm_object;  //configuration of environment
-    `uvm_object_utils(apb_config)
-
-      function new(string name = "apb_config");
-        super.new(name);
-      endfunction
-
-      uvm_active_passive_enum is_active = UVM_ACTIVE;
-endclass
